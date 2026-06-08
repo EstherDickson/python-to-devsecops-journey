6ad5bfe9ddc317ef2bc3a6d1cc7c9d28c8cba7a7
Esther Dickson <estherdickson36@gmail.com>
2026-06-08 23:48:38 +0100

    Python Fundamentals: Variables and Conditionals

---
+++ 01_python_fundamentals.py
@@ -0,0 +1,44 @@
+#PROJECT 1: VARIABLES AND DATA TYPES
+name = "Esther"
+age = 28
+country = "Nigeria"
+hobby = "Learning"
+is_student = True
+
+print(f"Name Type: {type(name)}")
+print(f"Age Type: {type(age)}")
+print(f"Country Type: {type(country)}")
+print(f"Hobby Type: {type(hobby)}")
+print(f"is_student Type: {type(is_student)}")
+
+if is_student:
+    print("Yes")
+else:
+    print("No")
+
+#PROJECT 2: MORE DATA TYPES
+tech_name = "Cipher_Reign"
+tech_age = 30
+python_experience = 0.6
+tech_newbie = False
+print(f"Tech_name Type: {type(tech_name)}")
+print(f"Tech_age Type: {type(tech_age)}")
+print(f"Python_experience Type: {type(python_experience)}")
+print(f"Tech_newbie Type: {type(tech_newbie)}")
+
+if tech_newbie:
+    print("Yes")
+else:
+    print("No")
+
+#PROJECT 3: CONDITIONAL STATEMENTS
+is_coding = True
+if is_coding:
+    print("Yes")
+else:
+    print("No")
+is_sleeping = False
+if is_sleeping:
+    print("Yes")
+else:
+    print("No")
